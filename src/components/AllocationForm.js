99d47a1617c98d8b09251f1fdbf1f5d219ec1fd6
Taimoor <taimoortoor@gmail.com>
2023-01-17 03:35:46 -0500

    Basic functionality added.

--- src/components/AllocationForm.js
+++ src/components/AllocationForm.js
@@ -3,7 +3,7 @@ import React, { useContext, useState } from 'react';
 import { AppContext } from '../context/AppContext';
 
 const AllocationForm = (props) => {
-    const { dispatch,remaining  } = useContext(AppContext);
+    const { dispatch,remaining, currency  } = useContext(AppContext);
 
     const [name, setName] = useState('');
     const [cost, setCost] = useState('');
@@ -12,7 +12,7 @@ const AllocationForm = (props) => {
     const submitEvent = () => {
 
             if(cost > remaining) {
-                alert("The value cannot exceed remaining funds  £"+remaining);
+                alert(`The value cannot exceed remaining funds: ${currency} ${remaining}`);
                 setCost("");
                 return;
             }
@@ -60,6 +60,9 @@ const AllocationForm = (props) => {
                 <option value="Reduce" name="Reduce">Reduce</option>
                   </select>
 
+                  <div className="input-group-prepend" style={{ marginLeft: '2rem' }}>
+                <label className="input-group-text" htmlFor="cost">{currency}</label>
+                </div>
                     <input
                         required='required'
                         type='number'
---
+++ src/components/Currency.js
@@ -0,0 +1,27 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const Currency = () => {
+  const {dispatch } = useContext(AppContext);
+
+	const changeCurrency = (val)=>{
+			dispatch({
+				type: 'CHG_CURRENCY',
+				payload: val,
+			})
+	}
+	
+
+  return (
+		<div className='alert alert-secondary'>
+      <select name="currency" id="currency" onChange={(event)=>changeCurrency(event.target.value)}>
+        <option value="£">£</option>
+        <option value="$">$</option>
+        <option value="€">€</option>
+        <option value="₹">₹</option>
+      </select>		
+    </div>
+	);
+};
+
+export default Currency;
